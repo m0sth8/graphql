26b5efd6adf37fb2a22576b6d7d594dc316b2cba
m0sth8 <m0sth8@gmail.com>
2020-09-09 19:51:08 -0700

    Update imports

--- query.go
+++ query.go
@@ -7,7 +7,7 @@ import (
 	"reflect"
 	"sort"
 
-	"github.com/shurcooL/graphql/ident"
+	"github.com/m0sth8/graphql/ident"
 )
 
 func constructQuery(v interface{}, variables map[string]interface{}) string {
